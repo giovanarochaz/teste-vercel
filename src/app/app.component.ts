c346c6ddf25797319cadb0f3f44b3ede6178d8c8
giovanarochaz <giovana.souza128@etec.sp.gov.br>
2024-06-17 10:39:16 -0300

    correção

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { InicioComponent } from '../pages/inicio/inicio.component';
 import { TemplateComponent } from '../components/template/template.component';
 
 @Component({
